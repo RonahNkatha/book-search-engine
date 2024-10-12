e2dc52aad278f994dfadd0024fedaee8a66001bf
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-10-12 19:30:56 -0400

    Initial commit

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,18 @@
+// import Tech, { ITech } from '../models/Tech.js';
+
+import User from "../models/User";
+import Book from "../models/Book";
+import { signToken } from "../services/auth.js";
+
+// import Matchup, { IMatchup } from '../models/Matchup.js';
+
+const resolvers = {
+  Query: {
+    
+  },
+  Mutation: {
+    
+  },
+};
+
+export default resolvers;
