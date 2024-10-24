01e467ae93c98fdcc45534654071f73faf3cb111
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-10-24 17:38:44 -0400

    fixed the path

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "node:path";
+import path from "path";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import db from "./config/connection.js";
@@ -40,10 +40,10 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
 
     app.get("*", (_req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
     });
   }
 
